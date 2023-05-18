a4b92fe2b8c1ffaf89d335d820dabfe50d7dc0e2
CHRIS HANSHIA C <113849749+ChrisHanshia@users.noreply.github.com>
2023-05-18 17:01:20 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,29 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    date_of_birth = Column(String)
+    register_number = Column(Integer, unique=True)
+    phone_number = Column(Integer)
+    address = Column(String)
+    course = Column(String)
+    gender = Column(String)
+
+class Todos(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
