35706f25323f9eccdcc96ad29516300c939ddd35
CHRIS HANSHIA C <113849749+ChrisHanshia@users.noreply.github.com>
2023-05-25 11:59:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import auth, admin, users
+from routers import auth, users
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
-app.include_router(admin.router)
-app.include_router(users.router)
