04711f3963960d79d99e72ddd9424f511dc2a1b3
CHRIS HANSHIA C <113849749+ChrisHanshia@users.noreply.github.com>
2023-05-31 12:10:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000", "http://localhost:4200", "https://your-render-domain.com", "https://onrender.com/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
